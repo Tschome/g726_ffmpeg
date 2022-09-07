163f7efec5e24f194d212bf4102c3e94170afe97
tschome <1054367842@qq.com>
2022-09-07 12:02:27 +0800

    从FFMPEG5.1源码库里移植出G726le编码解码器算法 现仅只支持16位宽的音频数据

---
+++ g726codec.c
@@ -0,0 +1,539 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include "g726codec.h"
+
+G726Context *pDeContext = NULL;
+G726Context *pEnContext = NULL;
+
+/**
+ * Paragraph 4.2.2 page 18: Adaptive quantizer.
+ */
+static inline uint8_t quant(G726Context* c, int d)
+{
+	int sign, exp, i, dln;
+
+	sign = i = 0;
+	if (d < 0) {
+		sign = 1;
+		d = -d;
+	}
+    exp = av_log2_16bit(d);
+    dln = ((exp<<7) + (((d<<7)>>exp)&0x7f)) - (c->y>>2);
+
+    while (c->tbls.quant[i] < INT_MAX && c->tbls.quant[i] < dln)
+        ++i;
+
+    if (sign)
+        i = ~i;
+    if (c->code_size != 2 && i == 0) /* I'm not sure this is a good idea */
+        i = 0xff;
+
+    return i;
+}
+
+/**
+ * Paragraph 4.2.3 page 22: Inverse adaptive quantizer.
+ */
+static inline int16_t inverse_quant(G726Context* c, int i)
+{
+    int dql, dex, dqt;
+
+    dql = c->tbls.iquant[i] + (c->y >> 2);
+    dex = (dql>>7) & 0xf;        /* 4-bit exponent */
+    dqt = (1<<7) + (dql & 0x7f); /* log2 -> linear */
+    return (dql < 0) ? 0 : ((dqt<<dex) >> 7);
+}
+
+/**
+ * Clip a signed integer into the -(2^p),(2^p-1) range.
+ * @param  a value to clip
+ * @param  p bit position to clip at
+ * @return clipped value
+ */
+static inline int av_clip_intp2(int a, int p)
+{
+    if (((unsigned)a + (1 << p)) & ~((2 << p) - 1))
+        return (a >> 31) ^ ((1 << p) - 1);
+    else
+        return a;
+}
+
+static int16_t g726_decode(G726Context* c, int I)
+{
+    int dq, re_signal, pk0, fa1, i, tr, ylint, ylfrac, thr2, al, dq0;
+    Float11 f;
+    int I_sig= I >> (c->code_size - 1);
+
+	dq = inverse_quant(c, I);
+
+    /* Transition detect */
+    ylint = (c->yl >> 15);
+    ylfrac = (c->yl >> 10) & 0x1f;
+    thr2 = (ylint > 9) ? 0x1f << 10 : (0x20 + ylfrac) << ylint;
+    tr= (c->td == 1 && dq > ((3*thr2)>>2));
+
+    if (I_sig)  /* get the sign */
+        dq = -dq;
+    re_signal = (int16_t)(c->se + dq);
+
+    /* Update second order predictor coefficient A2 and A1 */
+    pk0 = (c->sez + dq) ? sgn(c->sez + dq) : 0;
+    dq0 = dq ? sgn(dq) : 0;
+    if (tr) {
+        c->a[0] = 0;
+        c->a[1] = 0;
+        for (i=0; i<6; i++)
+            c->b[i] = 0;
+    } else {
+        /* This is a bit crazy, but it really is +255 not +256 */
+        fa1 = av_clip_intp2((-c->a[0]*c->pk[0]*pk0)>>5, 8);
+
+        c->a[1] += 128*pk0*c->pk[1] + fa1 - (c->a[1]>>7);
+        c->a[1] = av_clip(c->a[1], -12288, 12288);
+        c->a[0] += 64*3*pk0*c->pk[0] - (c->a[0] >> 8);
+        c->a[0] = av_clip(c->a[0], -(15360 - c->a[1]), 15360 - c->a[1]);
+
+        for (i=0; i<6; i++)
+            c->b[i] += 128*dq0*sgn(-c->dq[i].sign) - (c->b[i]>>8);
+    }
+
+    /* Update Dq and Sr and Pk */
+    c->pk[1] = c->pk[0];
+    c->pk[0] = pk0 ? pk0 : 1;
+    c->sr[1] = c->sr[0];
+    i2f(re_signal, &c->sr[0]);
+    for (i=5; i>0; i--)
+        c->dq[i] = c->dq[i-1];
+    i2f(dq, &c->dq[0]);
+    c->dq[0].sign = I_sig; /* Isn't it crazy ?!?! */
+
+    c->td = c->a[1] < -11776;
+
+    /* Update Ap */
+    c->dms += (c->tbls.F[I]<<4) + ((- c->dms) >> 5);
+    c->dml += (c->tbls.F[I]<<4) + ((- c->dml) >> 7);
+    if (tr)
+        c->ap = 256;
+    else {
+        c->ap += (-c->ap) >> 4;
+        if (c->y <= 1535 || c->td || abs((c->dms << 2) - c->dml) >= (c->dml >> 3))
+            c->ap += 0x20;
+    }
+
+    /* Update Yu and Yl */
+    c->yu = av_clip(c->y + c->tbls.W[I] + ((-c->y)>>5), 544, 5120);
+    c->yl += c->yu + ((-c->yl)>>6);
+
+    /* Next iteration for Y */
+    al = (c->ap >= 256) ? 1<<6 : c->ap >> 2;
+    c->y = (c->yl + (c->yu - (c->yl>>6))*al) >> 6;
+
+    /* Next iteration for SE and SEZ */
+    c->se = 0;
+    for (i=0; i<6; i++)
+        c->se += mult(i2f(c->b[i] >> 2, &f), &c->dq[i]);
+    c->sez = c->se >> 1;
+    for (i=0; i<2; i++)
+        c->se += mult(i2f(c->a[i] >> 2, &f), &c->sr[i]);
+    c->se >>= 1;
+
+	return av_clip(re_signal * 4, -0xffff, 0xffff);
+}
+
+static int g726_reset(G726Context *c)
+{
+    int i;
+
+    c->tbls = G726Tables_pool[c->code_size - 2];
+    for (i=0; i<2; i++) {
+        c->sr[i].mant = 1<<5;
+        c->pk[i] = 1;
+    }
+    for (i=0; i<6; i++) {
+        c->dq[i].mant = 1<<5;
+    }
+    c->yu = 544;
+    c->yl = 34816;
+
+    c->y = 544;
+
+    return 0;
+}
+
+static int16_t g726_encode(G726Context* c, int16_t sig)
+{
+    uint8_t i;
+
+    i = av_mod_uintp2(quant(c, sig/4 - c->se), c->code_size);
+    g726_decode(c, i);
+
+    return i;
+}
+
+
+
+/**
+ * Initialize the PutBitContext s.
+ *
+ * @param buffer the buffer where to put bits
+ * @param buffer_size the size in bytes of buffer
+ */
+static inline void init_put_bits(PutBitContext *s, uint8_t *buffer, int buffer_size)
+{
+    if (buffer_size < 0) {
+        buffer_size = 0;
+        buffer      = NULL;
+    }
+
+    s->buf          = buffer;
+    s->buf_end      = s->buf + buffer_size;
+    s->buf_ptr      = s->buf;
+    s->bit_left     = BUF_BITS;
+    s->bit_buf      = 0;
+}
+
+static inline void put_bits_le(PutBitContext *s, int n, uint32_t value)
+{   
+    uint32_t bit_buf;
+    int bit_left;
+    
+    bit_buf  = s->bit_buf;
+    bit_left = s->bit_left;
+        
+    bit_buf |= value << (BUF_BITS - bit_left);
+    if (n >= bit_left) {
+        if (s->buf_end - s->buf_ptr >= sizeof(uint32_t)) {
+            AV_WL32(s->buf_ptr, bit_buf);
+            
+			s->buf_ptr += sizeof(uint32_t);
+        } else {
+            printf("Internal error, put_bits buffer too small\n");
+            return ;
+        }
+        bit_buf     = value >> bit_left;
+        bit_left   += BUF_BITS;
+    }
+    bit_left -= n;
+
+    s->bit_buf  = bit_buf;
+    s->bit_left = bit_left;
+}
+
+
+static inline void flush_put_bits_le(PutBitContext *s)
+{
+    while (s->bit_left < BUF_BITS) {
+        //av_assert0(s->buf_ptr < s->buf_end);
+        *s->buf_ptr++ = s->bit_buf;
+        s->bit_buf  >>= 8;
+        s->bit_left  += 8;
+    }
+    s->bit_left = BUF_BITS;
+    s->bit_buf  = 0;
+}
+
+/* Interfacing to the libavcodec */
+int g726_encode_init()
+{
+	G726Context * c = NULL;
+	c = (G726Context *)malloc(sizeof(G726Context));
+	if(!c){
+		printf("param c is NULL\n");
+		return -1;
+	}
+
+    c->little_endian = 1;
+    c->code_size = 2;
+
+    g726_reset(c);
+	pEnContext = c;
+
+    return 0;
+}
+
+int g726_encode_frame(uint8_t *out, int inS16Len, const int16_t *in)
+{
+	PutBitContext pb;
+	int i = 0;
+
+	G726Context *c = pEnContext;
+	if(!c){
+		return -1;
+	}
+
+	if(!in || (inS16Len <= 0) || !out || (in && inS16Len <= 0)){
+		printf(" in or inS16Len or out is NULL\n");
+		return -1;
+	}
+
+	init_put_bits(&pb, out, inS16Len);
+	for(i = 0; i < inS16Len; i++)
+		put_bits_le(&pb, 2, g726_encode(c, *in++));
+
+	flush_put_bits_le(&pb);
+
+	return inS16Len/4;
+}
+
+int g726_encode_destroy()
+{
+	G726Context * c = pEnContext;
+	if(!c){
+		printf("param c is already NULL\n");
+		return 0;
+	}
+
+	free(c);
+
+    return 0;
+}
+
+
+/**
+ * Initialize GetBitContext.
+ * @param buffer bitstream buffer, must be AV_INPUT_BUFFER_PADDING_SIZE bytes
+ *        larger than the actual read bits because some optimized bitstream
+ *        readers read 32 or 64 bit at once and could read over the end
+ * @param bit_size the size of the buffer in bits
+ * @return 0 on success, AVERROR_INVALIDDATA if the buffer_size would overflow.
+ */
+static inline int init_get_bits(GetBitContext *s, const uint8_t *buffer,int bit_size)
+{
+    int buffer_size;
+    int ret = 0;
+
+    if (bit_size >= INT_MAX - FFMAX(7, 64*8) || bit_size < 0 || !buffer) {
+        bit_size    = 0;
+        buffer      = NULL;
+        ret         = -1;
+    }
+
+    buffer_size = (bit_size + 7) >> 3;
+
+    s->buffer             = buffer;
+    s->size_in_bits       = bit_size;
+    s->size_in_bits_plus8 = bit_size + 8;
+    s->buffer_end         = buffer + buffer_size;
+    s->index              = 0;
+    return ret;
+}
+
+static inline unsigned int get_bits_le(GetBitContext *s, int n)
+{
+	int tmp;
+	unsigned int index = s->index;
+	unsigned int cache = AV_RL32(s->buffer + (index >> 3)) >> (index & 7);
+	tmp = zero_extend(cache, n);
+	index += n;
+	s->index = index;
+
+	return tmp;
+}
+
+int g726_decode_init()
+{
+	G726Context * c = NULL;
+	c = (G726Context *)malloc(sizeof(G726Context));
+	if(!c){
+		printf("param c is NULL\n");
+		return -1;
+	}
+
+    c->little_endian = 1;
+    c->code_size = 2;
+
+    g726_reset(c);
+
+	pDeContext = c;
+    return 0;
+}
+
+int g726_decode_frame(const uint8_t *in, int inU8Len, int16_t *out)
+{
+	int out_samples, ret;
+	GetBitContext gb;
+	
+	G726Context *c = pDeContext;
+	if(!c){
+		printf(" c is NULL\n");
+		return -1;
+	}
+
+	if(!in || (inU8Len <= 0) || !out || (in && inU8Len <= 0)){
+		printf(" in or inU8Len or out is NULL\n");
+		return -1;
+	}
+
+	init_get_bits(&gb, in, inU8Len * 8);
+	out_samples = inU8Len * 8 / c->code_size;
+	while(out_samples--){
+		*out++ = g726_decode(c, get_bits_le(&gb, c->code_size));
+
+	//printf("func:%s, line:%d\n", __func__, __LINE__);
+	}
+	return inU8Len;
+}
+
+static void g726_decode_flush()
+{
+	G726Context *c = pDeContext;
+	if(!c){
+		printf(" c is NULL\n");
+		return ;
+	}
+
+    g726_reset(c);
+}
+
+int g726_decode_destroy()
+{
+	G726Context * c = pDeContext;
+	if(!c){
+		printf("param c is already NULL\n");
+		return 0;
+	}
+
+	free(c);
+
+    return 0;
+}
+
+#if 0
+int main(int argc, char **argv)
+{
+   	const char *outfilename, *filename;
+	if (argc <= 2) {
+		fprintf(stderr, "Usage: %s <input file> <output file>\n", argv[0]);
+		exit(0);
+	}   
+	filename    = argv[1];
+	outfilename = argv[2];
+
+	FILE *f, *outfile;
+	f = fopen(filename, "rb");
+	if (!f) {
+		fprintf(stderr, "Could not open %s\n", filename);
+		exit(1);
+	}
+	outfile = fopen(outfilename, "wb");
+	if (!outfile) {
+		exit(1);
+	}
+
+	int   data_size;
+	uint8_t *inbuf = (uint8_t *)malloc(8000 * sizeof(short) * 4096 * 4/ 1000/ 8);
+	if(inbuf == NULL){
+		fprintf(stderr, "Could not alloc inbuf \n");
+		return -1;
+	}
+
+	int16_t *data = (int16_t *)malloc(8000 * sizeof(short) * 4096 *4/ 1000);
+	if(data == NULL){
+		fprintf(stderr, "Could not alloc inbuf \n");
+		return -1;
+	}
+	int ret = 0;
+
+#if 0
+//解码
+	if(0 != g726_decode_init())
+	{
+		fprintf(stderr, "Could not init decodec \n");
+		return -1;
+	}
+
+	while(1)
+	{
+		data_size = fread(inbuf, 1, 8000/**sizeof(short)*/ * 40 / 1000/8, f);
+		if(data_size <= 0){
+			fprintf(stderr, "Could not read :%s\n", strerror(errno));
+			break;
+		}
+
+		int i = 0;
+	/*	for(i = 0; i < data_size; i++){
+			if(i % 8 == 0)
+				printf("\n");
+			printf("%02x ", inbuf[i]);
+		}*/
+		printf("\n");
+		printf("line:%d, data_size:%d\n", __LINE__, data_size);
+
+
+
+		ret = g726_decode_frame((const uint8_t *)inbuf, data_size, (int16_t *)data);
+		if(ret != data_size)
+		{
+			printf("ret(%d) != datasize(%d)\n", ret, data_size);
+			return -1;
+		}
+
+		/*for(i = 0; i < ret*4; i++){
+			if(i % 8 == 0)
+				printf("\n");
+			printf("%08x ", data[i]);
+		}*/
+		printf("\n");
+		ret = fwrite(data, 2, ret*4, outfile);
+		printf("writed:%d  INT_MAX:%d\n", ret, INT_MAX);
+	}
+
+#else
+//编码
+	if(0 != g726_encode_init())
+	{
+		fprintf(stderr, "Could not init encodec \n");
+		return -1;
+	}
+
+	while(1)
+	{
+		data_size = fread(data, 1, 640, f);
+		if(data_size <= 0){
+			fprintf(stderr, "Could not read :%s\n", strerror(errno));
+			break;
+		}
+
+		int i = 0;
+	/*	
+		for(i = 0; i < data_size/2; i++){
+			if(i % 8 == 0)
+				printf("\n");
+			printf("%04x ", (uint16_t)data[i]);
+		}
+	*/	
+		printf("\n");
+		printf("line:%d, data_size:%d\n", __LINE__, data_size);
+
+		ret = g726_encode_frame((uint8_t *)inbuf, data_size/2, (const int16_t *)data);
+		if(ret != data_size/8)
+		{
+			printf("ret(%d) != datasize(%d)\n", ret, data_size/8);
+			return -1;
+		}
+/*
+		for(i = 0; i < ret; i++){
+			if(i % 8 == 0)
+				printf("\n");
+			printf("%02x ", inbuf[i]);
+		}
+*/
+		printf("\n");
+		ret = fwrite(inbuf, 1, data_size/8, outfile);
+		printf("writed:%d \n", ret);
+	}
+
+
+#endif
+
+	fclose(f);
+	fclose(outfile);
+
+	free(data);
+	free(inbuf);
+	return 0;
+}
+#endif
---
+++ g726codec.h
@@ -0,0 +1,183 @@
+/*
+ * Cut from ffmpeg, only suitable for 16 bits wide
+ * 
+ * CARE: It's different from CCITT's algorithm
+ * */
+
+
+#ifndef __G726_FFMPEG_H__
+#define __G726_FFMPEG_H__
+#include <stdint.h>
+#if 0
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long int uint64_t;
+#endif
+#define INT_MAX 2147483647
+#define FFMAX(a,b) ((a) > (b) ? (a) : (b))
+#define av_log2_16bit(x) (31 - __builtin_clz((x)|1))
+#define BUF_BITS (8 * sizeof(uint32_t))
+
+#define AV_WL32(p, val) do {                 \
+        uint32_t d = (val);                     \
+        ((uint8_t*)(p))[0] = (d);               \
+        ((uint8_t*)(p))[1] = (d)>>8;            \
+        ((uint8_t*)(p))[2] = (d)>>16;           \
+        ((uint8_t*)(p))[3] = (d)>>24;           \
+    } while(0)
+#define AV_RL32(x)                                \
+    (((uint32_t)((const uint8_t*)(x))[3] << 24) |    \
+               (((const uint8_t*)(x))[2] << 16) |    \
+               (((const uint8_t*)(x))[1] <<  8) |    \
+                ((const uint8_t*)(x))[0])
+
+/**
+ * G.726 11-bit float.
+ * G.726 Standard uses rather odd 11-bit floating point arithmetic for
+ * numerous occasions. It's a mystery to me why they did it this way
+ * instead of simply using 32-bit integer arithmetic.
+ */
+typedef struct Float11 {
+    uint8_t sign;   /**< 1 bit sign */
+    uint8_t exp;    /**< 4 bits exponent */
+    uint8_t mant;   /**< 6 bits mantissa */
+} Float11;
+
+typedef struct G726Tables {
+    const int* quant;         /**< quantization table */
+    const int16_t* iquant;    /**< inverse quantization table */
+    const int16_t* W;         /**< special table #1 ;-) */
+    const uint8_t* F;         /**< special table #2 */
+} G726Tables;
+
+static const int quant_tbl16[] =                  /**< 16kbit/s 2 bits per sample */
+           { 260, INT_MAX };
+static const int16_t iquant_tbl16[] =
+           { 116, 365, 365, 116 };
+static const int16_t W_tbl16[] =
+           { -22, 439, 439, -22 };
+static const uint8_t F_tbl16[] =
+           { 0, 7, 7, 0 };
+static const G726Tables G726Tables_pool[] =
+           {{ quant_tbl16, iquant_tbl16, W_tbl16, F_tbl16 }};
+
+typedef struct G726Context {
+    G726Tables tbls;    /**< static tables needed for computation */
+
+    Float11 sr[2];      /**< prev. reconstructed samples */
+    Float11 dq[6];      /**< prev. difference */
+    int a[2];           /**< second order predictor coeffs */
+    int b[6];           /**< sixth order predictor coeffs */
+    int pk[2];          /**< signs of prev. 2 sez + dq */
+
+    int ap;             /**< scale factor control */
+    int yu;             /**< fast scale factor */
+    int yl;             /**< slow scale factor */
+    int dms;            /**< short average magnitude of F[i] */
+    int dml;            /**< long average magnitude of F[i] */
+    int td;             /**< tone detect */
+
+    int se;             /**< estimated signal for the next iteration */
+    int sez;            /**< estimated second order prediction */
+    int y;              /**< quantizer scaling factor for the next iteration */
+    int code_size;
+    int little_endian;  /**< little-endian bitstream as used in aiff and Sun AU */
+} G726Context;
+
+typedef struct PutBitContext {
+    uint32_t bit_buf;
+    int bit_left;
+    uint8_t *buf, *buf_ptr, *buf_end;
+} PutBitContext;
+
+typedef struct GetBitContext {
+    const uint8_t *buffer, *buffer_end;
+    int index;
+    int size_in_bits;
+    int size_in_bits_plus8; 
+} GetBitContext;
+
+
+static inline unsigned int zero_extend(unsigned int val, unsigned int bits)
+{
+    return (val << ((8 * sizeof(int)) - bits)) >> ((8 * sizeof(int)) - bits);
+}
+
+static inline Float11* i2f(int i, Float11* f)
+{
+	f->sign = (i < 0);
+	if (f->sign)
+		i = -i;
+	f->exp = av_log2_16bit(i) + !!i;
+	f->mant = i? (i<<6) >> f->exp : 1<<5;
+	return f;
+}
+
+static inline int16_t mult(Float11* f1, Float11* f2)
+{
+	int res, exp;
+
+	exp = f1->exp + f2->exp;
+	res = (((f1->mant * f2->mant) + 0x30) >> 4);
+	res = exp > 19 ? res << (exp - 19) : res >> (19 - exp);
+	return (f1->sign ^ f2->sign) ? -res : res;
+}
+
+static inline int sgn(int value)
+{
+	return (value < 0) ? -1 : 1;
+}
+/**
+ * Clip a signed integer value into the amin-amax range.
+ * @param a value to clip
+ * @param amin minimum value of the clip range
+ * @param amax maximum value of the clip range
+ * @return clipped value
+ */
+static inline int av_clip(int a, int amin, int amax)
+{
+    if      (a < amin) return amin;
+    else if (a > amax) return amax;
+    else               return a;
+}
+/**
+ * Clear high bits from an unsigned integer starting with specific bit position
+ * @param  a value to clip
+ * @param  p bit position to clip at
+ * @return clipped value
+ */
+static inline uint8_t av_mod_uintp2(unsigned int a, unsigned int p)
+{
+    return a & ((1U << p) - 1);
+}
+
+
+
+
+int g726_encode_init();
+
+/**
+ * Encode some frames
+ * @param  out 		Data after encoding
+ * @param  inS16Len	Data len before encoding
+ * @param  in		Data  before encoding 
+ * @return clipped value
+ */
+int g726_encode_frame(uint8_t *out, int inS16Len, const int16_t *in);
+
+int g726_encode_destroy();
+
+int g726_decode_init();
+/**
+ * Decode some frames
+ * @param  in		Data  before decoding
+ * @param  inU8Len	Data len before decoding
+ * @param  out 		Data after decoding
+ * @return clipped value
+ */
+int g726_decode_frame(const uint8_t *in, int inU8Len, int16_t *out);
+
+int g726_decode_destroy();
+
+#endif
